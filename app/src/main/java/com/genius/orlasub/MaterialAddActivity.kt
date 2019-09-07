a6e883cf22026febc15fde17cb72ad61e3f40541
choidaye <soar_dy@naver.com>
2019-09-07 13:54:35 +0900

    add sub app

---
+++ app/src/main/java/com/genius/orlasub/MaterialAddActivity.kt
@@ -0,0 +1,12 @@
+package com.genius.orlasub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MaterialAddActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_material_add)
+    }
+}
