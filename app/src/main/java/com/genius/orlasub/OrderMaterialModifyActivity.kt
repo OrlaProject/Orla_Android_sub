f7f95b78fe9e13e2c586f6acaff05ba65b430e06
choidaye <soar_dy@naver.com>
2019-09-07 13:54:51 +0900

    add xml

--- app/src/main/java/com/genius/orlasub/OrderMaterialModifyActivity.kt
+++ app/src/main/java/com/genius/orlasub/OrderMaterialModifyActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.genius.orlasub.Data.MaterialListData
 import kotlinx.android.synthetic.main.activity_order_material_modify.*
+import org.jetbrains.anko.startActivity
 
 
 class OrderMaterialModifyActivity : AppCompatActivity() {
@@ -17,9 +18,9 @@ class OrderMaterialModifyActivity : AppCompatActivity() {
 
         var materiallist: ArrayList<MaterialListData> = ArrayList()
         materiallist.add(MaterialListData("사과","등록번호 : 031300001"))
-        materiallist.add(MaterialListData("사과","등록번호 : 031300001"))
-        materiallist.add(MaterialListData("사과","등록번호 : 031300001"))
-        materiallist.add(MaterialListData("사과","등록번호 : 031300001"))
+        materiallist.add(MaterialListData("포도","등록번호 : 031300231"))
+        materiallist.add(MaterialListData("상추","등록번호 : 031303456"))
+        materiallist.add(MaterialListData("케일","등록번호 : 031302314"))
 
 
 
@@ -30,6 +31,16 @@ class OrderMaterialModifyActivity : AppCompatActivity() {
 
 
 
+        setOnBtnClickListener()
 
+
+
+
+    }
+
+    private fun setOnBtnClickListener() {
+        btn_ac_material_modify_add.setOnClickListener {
+            startActivity<MaterialAddActivity>()
+        }
     }
 }
